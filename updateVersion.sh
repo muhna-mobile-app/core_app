a81fcebf59a95edb42efb67b1450a1b74691a305
fillipeags <fillipe484@gmail.com>
2023-04-18 01:27:02 -0300

    feat: update pubspec version

--- updateVersion.sh
+++ updateVersion.sh
@@ -4,7 +4,7 @@
 VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 
 # Update the version in pubspec.yaml
-sed -i "" "s/^version: .*/version: $VERSION/" pubspec.yaml
+sed -i "s/^version: .*/version: $VERSION/" pubspec.yaml
 
 # Print the old and new versions to the console
 echo "Old version: $(cat pubspec.yaml | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[", ]//g')"
