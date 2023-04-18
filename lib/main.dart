4687d42ebc7751a07a7d6c98df550b4054d50f56
kaio-nink <51462685+kaio-nink@users.noreply.github.com>
2023-04-17 23:30:44 -0300

    fix: update app title

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MuHNA',
+      title: 'MuHNA App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
