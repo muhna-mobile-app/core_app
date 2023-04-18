5e1a13fa4e53b7a48603d498e828b365c27e8c97
fillipeags <fillipe484@gmail.com>
2023-04-18 01:02:12 -0300

    fix: updat eci

---
+++ updateVersion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Read the version from package.json
+VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+
+# Update the version in pubspec.yaml
+sed -i "" "s/^version: .*/version: $VERSION/" pubspec.yaml
+
+# Print the old and new versions to the console
+echo "Old version: $(cat pubspec.yaml | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[", ]//g')"
+echo "New version: $VERSION"
